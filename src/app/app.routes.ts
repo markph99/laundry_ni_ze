69d0a8b5339c3c82159d93958f2170de54410cc5
markph99 <mjtacan99@gmail.com>
2025-01-23 13:16:25 +0800

    day 1

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,4 +6,12 @@ export const routes: Routes = [
     path: '',
     loadChildren: () => import('./content/content-routing.module').then((m) =>m.ContentRoutingModule)
   },
+  {
+    path: 'claim',
+    loadChildren: () => import('./claim/claim-routing.module').then((m) =>m.ClaimRoutingModule)
+  },
+  {
+    path: 'add_slip',
+    loadChildren: () => import('./add-slip/add-slip-routing.module').then((m) =>m.AddSlipRoutingModule)
+  }
 ];
